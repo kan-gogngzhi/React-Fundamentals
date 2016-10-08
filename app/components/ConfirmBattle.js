a502e7d2c076784ab5fbc33bd5090f1799a242dd
Gongzhi Kan <kangongzhi@gmail.com>
2016-10-08 11:13:18 +1100

    added transition addons

--- app/components/ConfirmBattle.js
+++ app/components/ConfirmBattle.js
@@ -8,32 +8,31 @@ const {space, transparentBg} = styles;
 const {PropTypes} = React;
 
 const ConfirmBattle = (props)=> {
-    return <Loading/>;
-    //return props.isLoading === true
-    //    ? <Loading/>
-    //    : <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
-    //    <h1>Confirm Players</h1>
-    //    <div className='col-sm-8 col-sm-offset-2'>
-    //        <UserDetailsWrapper header='Player 1'>
-    //            <UserDetails info={props.playersInfo[0]}/>
-    //        </UserDetailsWrapper>
-    //        <UserDetailsWrapper header='Player 2'>
-    //            <UserDetails info={props.playersInfo[1]}/>
-    //        </UserDetailsWrapper>
-    //    </div>
-    //    <div className='col-sm-8 col-sm-offset-2'>
-    //        <div className='col-sm-12' style={space}>
-    //            <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate
-    //                Battle!
-    //            </button>
-    //        </div>
-    //        <div className='col-sm-12' style={space}>
-    //            <Link to='/playerOne'>
-    //                <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
-    //            </Link>
-    //        </div>
-    //    </div>
-    //</div>;
+    return props.isLoading === true
+        ? <Loading/>
+        : <div className="jumbotron col-sm-12 text-center" style={transparentBg}>
+        <h1>Confirm Players</h1>
+        <div className='col-sm-8 col-sm-offset-2'>
+            <UserDetailsWrapper header='Player 1'>
+                <UserDetails info={props.playersInfo[0]}/>
+            </UserDetailsWrapper>
+            <UserDetailsWrapper header='Player 2'>
+                <UserDetails info={props.playersInfo[1]}/>
+            </UserDetailsWrapper>
+        </div>
+        <div className='col-sm-8 col-sm-offset-2'>
+            <div className='col-sm-12' style={space}>
+                <button type='button' className='btn btn-lg btn-success' onClick={props.onInitiateBattle}>Initiate
+                    Battle!
+                </button>
+            </div>
+            <div className='col-sm-12' style={space}>
+                <Link to='/playerOne'>
+                    <button type='button' className='btn btn-lg btn-danger'>Reselect Players</button>
+                </Link>
+            </div>
+        </div>
+    </div>;
 };
 
 ConfirmBattle.propTypes = {
